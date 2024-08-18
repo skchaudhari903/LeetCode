93030861b70e89f28b3c2c9a01a90dcc2af32d38
Shubham Chaudhary <skchaudhari903@gmail.com>
2024-08-18 22:22:42 +0530

    Create 58-Length-of-Last-Word.java

---
+++ Daily Question/58-Length-of-Last-Word.java
@@ -0,0 +1,21 @@
+/**
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+*/
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        s = s.trim();
+        for(int i = s.length()-1; i >= 0; i--)
+        {
+            if(s.charAt(i) != ' ')
+            {
+                length++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return length;
+    }
+}
