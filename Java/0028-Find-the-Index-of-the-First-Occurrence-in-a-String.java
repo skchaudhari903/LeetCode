016196b225266caa7d9456e0f076c15edd3fa400
Shubham Chaudhary <skchaudhari903@gmail.com>
2024-11-19 23:24:20 +0530

    Update 0028-Find-the-Index-of-the-First-Occurrence-in-a-String.java

--- Java/0028-Find-the-Index-of-the-First-Occurrence-in-a-String.java
+++ Java/0028-Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -5,8 +5,23 @@ Output: 0
 Explanation: "sad" occurs at index 0 and 6.
 The first occurrence is at index 0, so we return 0.
  */
+
 class Solution {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
 }
+/** 
+// Alternate Solution
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        for (int i = 0; i <= m - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+*/
