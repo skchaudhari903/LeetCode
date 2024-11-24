bea7bc5d5a768c05eb7eca2e216b1681effd28d1
Shubham Chaudhary <skchaudhari903@gmail.com>
2024-11-25 00:36:52 +0530

    Update 0035-Search-Insert-Position.java

--- Java/0035-Search-Insert-Position.java
+++ Java/0035-Search-Insert-Position.java
@@ -5,11 +5,35 @@ Output: 2
  */
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] >= target)
-                return i;
-        }
-        return nums.length;
+        /**
+            for(int i = 0; i < nums.length; i++)
+            {
+                if(nums[i] >= target)
+                    return i;
+            }
+            return nums.length;
+         */
+         // O(log n) complexity
+         int start = 0;
+         int mid = -1;
+         int end = nums.length-1;
+
+         while(start <= end)
+         {
+            mid = start + (end - start) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid -1;
+            }
+         }
+        return start;
     }
 }
