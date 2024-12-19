48100a7e63e50bc1d5cfc537e8b96ce63b948312
Shubham Chaudhary <skchaudhari903@gmail.com>
2024-12-19 23:10:44 +0530

    Create 0175-Combine-Two-Tables.sql

---
+++ MySQL/0175-Combine-Two-Tables.sql
@@ -0,0 +1,31 @@
+# Write your MySQL query statement below
+/*
+Table: Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+
+Table: Address
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+Return the result table in any order.
+*/
+
+select p.firstName, p.lastName, a.city, a.state 
+from Person p LEFT JOIN Address a on p.personId = a.personId;
